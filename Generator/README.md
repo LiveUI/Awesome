7604745af823ee2b4e88587b064111ca23a1e1b0
Christopher Mühl <christopher@padarom.io>
2018-06-22 09:50:18 +0200

    Add latest assets

--- Generator/README.md
+++ Generator/README.md
@@ -1,8 +1,4 @@
 # Generator
-
-This generator is not built for performance, so some things are not done as efficiently as possible.
-
-### Use
 You need to have `swift` installed in order to compile Swift binaries from the command line.
 
 To create the assets, run `./generate.sh`.
--- Generator/generate.sh
+++ Generator/generate.sh
@@ -8,7 +8,12 @@ swift build
 ./.build/x86_64-apple-macosx10.10/debug/Generator
 
 # Download FontAwesome Free assets
+# TODO: Find newest version number
+curl https://use.fontawesome.com/releases/v5.1.0/fontawesome-free-5.1.0-web.zip --output fontawesome-free.zip
+unzip -joq ./fontawesome-free.zip -d FontAwesome
 
 # Copy assets and enums to target directories
 cp Output/Awesome.swift ../Classes/Enums
 cp Output/AwesomePro.swift ../Classes/Enums
+
+cp ./FontAwesome/*.ttf ../Assets
