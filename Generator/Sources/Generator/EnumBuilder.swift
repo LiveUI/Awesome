24af119ee38224668457b40a9805b77067533680
Christopher Mühl <christopher@padarom.io>
2018-07-25 20:41:14 +0200

    Replace tabstops with spaces in generator

--- Generator/Sources/Generator/EnumBuilder.swift
+++ Generator/Sources/Generator/EnumBuilder.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 func buildEnum (_ name: String, from styles: [String: [Icon]]) -> String {
-	let formatter = DateFormatter()
-	formatter.dateFormat = "dd/MM/yyyy HH:mm"
-	
+    let formatter = DateFormatter()
+    formatter.dateFormat = "dd/MM/yyyy HH:mm"
+
     var content = ""
     content += "//\n//  \(name).swift\n//  AwesomeEnum\n//\n//  Originally created by Ondrej Rafaj on 13/10/2017.\n"
     content += "//  Copyright © 2017 manGoweb UK. All rights reserved.\n//\n"
-	content += "//  This file has been auto-generated on \(formatter.string(from: Date())).\n\nimport Foundation\n\npublic struct \(name) {\n"
+    content += "//  This file has been auto-generated on \(formatter.string(from: Date())).\n\nimport Foundation\n\npublic struct \(name) {\n"
 
     for (var style, list) in styles {
-		if style == "brands" { style = "brand" }
-		
+        if style == "brands" { style = "brand" }
+
         content += "\n    public enum \(style): String, Amazing {\n"
         var names = [String]()
         for icon in list {
@@ -20,12 +20,12 @@ func buildEnum (_ name: String, from styles: [String: [Icon]]) -> String {
             if Int(name.prefix(1)) != nil { name = "fa\(name)" }
             names.append(name)
 
-			content += "        case \(name.isKeyword ? "`\(name)`" : name) = \"\\u{\(icon.unicode)}\"\n"
+            content += "        case \(name.isKeyword ? "`\(name)`" : name) = \"\\u{\(icon.unicode)}\"\n"
         }
 
         content += "\n        public static var all: [\(style)] {\n            return [ \(names.map {"\(style).\($0)"}.joined(separator: ", ")) ]\n        }\n"
-		content += "\n        public static var keys: [String] {\n            return [ \(list.map {"\"\($0.name)\""}.joined(separator: ", ")) ]\n        }\n"
-		content += "\n        public static var labels: [String] {\n            return [ \(list.map {"\"\($0.name.split(separator: "-").map { $0.firstUppercased() }.joined(separator: " "))\""}.joined(separator: ", ")) ]\n        }\n"
+        content += "\n        public static var keys: [String] {\n            return [ \(list.map {"\"\($0.name)\""}.joined(separator: ", ")) ]\n        }\n"
+        content += "\n        public static var labels: [String] {\n            return [ \(list.map {"\"\($0.name.split(separator: "-").map { $0.firstUppercased() }.joined(separator: " "))\""}.joined(separator: ", ")) ]\n        }\n"
         content += "\n        public var code: String {\n            return rawValue\n        }\n"
         content += "\n        public var fontType: AwesomeFont {\n            return \(name).Font.\(style)\n        }\n"
 
