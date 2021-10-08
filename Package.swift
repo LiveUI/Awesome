2b16af1baf804fdc30ac14fae29050ae23a7844c
sarath <sarath.d02@gmail.com>
2021-10-08 22:11:57 +0530

    updated .gitignore file

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Awesome",
+    platforms: [.iOS(.v9), .macOS(.v10_11), .watchOS(.v2), .tvOS(.v9)],
+    products: [
+        .library(
+            name: "Awesome",
+            targets: ["Awesome"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Awesome",
+            dependencies: [],
+            path: "Classes",
+            resources: [
+                .process("Assets"),
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
